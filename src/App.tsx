edc68cd11d90f465b01949f03502da0d431bae2c
Nikhil <nkarnwal07@gmail.com>
2022-12-25 21:25:52 +0530

    transpiling completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import * as esbuild from "esbuild-wasm";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = React.useState("");
+  const [code, setCode] = React.useState("");
+
+  const ref = React.useRef<any>();
+
+  React.useEffect(() => {
+    startService();
+  }, []);
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
+  const submitCode = async () => {
+    if (!ref.current) return;
+    const result = await ref.current.transform(input, {
+      loader: "jsx",
+      target: "es2015",
+    });
+    setCode(result.js);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={submitCode}>Submit</button>
+      <pre>{code}</pre>
     </div>
   );
 }
