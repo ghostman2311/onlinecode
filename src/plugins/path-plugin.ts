dfa3ab0c1b7237120f216999c0d807401c9300cd
Nikhil <nkarnwal07@gmail.com>
2023-01-01 22:37:54 +0530

    resolving nested paths

--- src/plugins/path-plugin.ts
+++ src/plugins/path-plugin.ts
@@ -12,9 +12,10 @@ export const unpkgPathPlugin = () => {
           return { path: args.path, namespace: "a" };
         } 
         else if(args.path.includes('./') || args.path.includes('../')){
+          console.log(args, 'args')
           return {
             namespace:'a',
-            path: new URL(args.path, args.importer + '/').href
+            path: new URL(args.path, 'https://unpkg.com' +  args.resolveDir + '/').href
           }
         }
           return{
@@ -31,15 +32,16 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              const message = require('medium-test-pkg');
+              const message = require('nested-test-pkg');
               console.log(message);
             `,
           }; 
         } else {
-          const {data} = await axios.get(args.path)
+          const {data, request} = await axios.get(args.path)
           return {
             loader:'jsx',
-            contents:data
+            contents:data, 
+            resolveDir: new URL('./', request.responseURL).pathname
           }
         }
       });
