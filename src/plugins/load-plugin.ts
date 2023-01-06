d428b7e3176fdca72f78d31c358411cc1611988f
Nikhil <nkarnwal07@gmail.com>
2023-01-06 15:44:35 +0530

    caching for big performance completed

--- src/plugins/load-plugin.ts
+++ src/plugins/load-plugin.ts
@@ -11,29 +11,44 @@ const unpkgLoadPlugin =(inputCode:string) => {
     return {
         name:'unpkg-load-plugin',
         setup: (build: esbuild.PluginBuild) => {
+
+        build.onLoad({filter: /(^index\.js$)/}, async () => {
+          return {
+            loader:'jsx',
+            contents:inputCode
+          }
+        })
+
+      build.onLoad({filter: /.*/}, async (args:any) => {
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+        if(cachedResult) {
+          return cachedResult;
+        }
+      })
+
+        build.onLoad({filter: /.css$/}, async (args: any) => {
+          const { data, request } = await axios(args.path);
+          const escapedData  = data.replace(/\n/g, '').replace(/"/g,'\\"' ).replace(/'/g, "\\'");
+          const contents = `
+            const style = document.createElement('style');
+            style.innerText = '${escapedData}';
+            document.head.appendChild(style);
+          `;
+          const result:esbuild.OnLoadResult = {
+            loader:'jsx',
+            contents,
+            resolveDir: new URL('./', request.responseURL).pathname
+          }
+          await fileCache.setItem(args.path, result);
+          return result;
+        })
         build.onLoad({ filter: /.*/ }, async (args: any) => {
             console.log("onLoad", args);
-    
-            if (args.path === "index.js") {
-              return {
-                loader: "jsx",
-                contents:inputCode,
-              }; 
-            } 
-              const cachedResult = await fileCache.getItem(args.path);
-            //   if(cachedResult) {
-            //     return cachedResult;
-            //   }
+             
               const { data, request } = await axios(args.path);
-              const fileType  = args.path.match(/.css$/) ? 'css' : 'jsx';
-              const contents = fileType === 'css' ? `
-                const style = document.createElement('style');
-                style.innerText = 'body {background-color:"red"}';
-                document.head.appendChild(style);
-              ` : data
-              const result = {
+              const result:esbuild.OnLoadResult = {
                 loader:'jsx',
-                contents,
+                contents:data,
                 resolveDir: new URL('./', request.responseURL).pathname
               }
               await fileCache.setItem(args.path, result);
